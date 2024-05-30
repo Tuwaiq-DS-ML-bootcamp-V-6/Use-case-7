45ce58800c12403cec06092ec18ec6ed94787193
Omar-Alhuwaishel <143610489+Omar-Alhuwaishel@users.noreply.github.com>
2024-05-30 07:44:49 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 X = pd.read_csv('Data/DBSCAN.csv')
-fig = px.scatter(X, x='assists', y='position_encoded', color='DBSCAN',
+fig = px.scatter(X, x='assists', y='goals', color='DBSCAN',
                  title='Scatter plot of Assists vs. Position Encoded',
                  labels={'assists': 'Assists', 'position_encoded': 'Position Encoded'})
 
