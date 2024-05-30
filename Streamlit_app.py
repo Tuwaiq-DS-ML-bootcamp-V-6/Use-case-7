7b4864f1ac071f22b9105223a55b417ef3267f58
Omar-Alhuwaishel <143610489+Omar-Alhuwaishel@users.noreply.github.com>
2024-05-30 07:46:24 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 X = pd.read_csv('Data/DBSCAN.csv')
-fig = px.scatter(X, x='assists', y='goals', color='DBSCAN',
-                 title='Scatter plot of Assists vs. Position Encoded',
-                 labels={'assists': 'Assists', 'position_encoded': 'Position Encoded'})
+fig = px.scatter(X, x='height', y='goals', color='DBSCAN',
+                 title='Scatter plot of height vs. goals',
+                 labels={'height': 'Height', 'goals': 'Goals'})
 
 # Display the plot in Streamlit
 st.plotly_chart(fig)
